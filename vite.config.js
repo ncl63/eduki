1df510e7ef24d1a11e86b5cba5d584f2a4568f95
ncl63 <97301321+ncl63@users.noreply.github.com>
2025-10-17 17:39:45 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// base = nom EXACT de ta repo
-export default defineConfig({
-  plugins: [react()],
-  base: '/travail-matija/',
-})
